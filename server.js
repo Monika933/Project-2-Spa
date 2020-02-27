c4971eabd29e4c0aaed5dbc637f4c3e986e32bf9
Monika Karanovic <monikakaranovic7@gmail.com>
2020-02-27 15:01:08 -0500

    first push

--- server.js
+++ server.js
@@ -20,3 +20,9 @@ db.sequelize.sync({ force: true }).then(function() {
       console.log("App listening on PORT " + PORT);
     });
   });
+
+  //Routes
+  //Basic route that sends the user first to the AJAX Page
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "view.html"));
+  });
